aa4d3bbf1f6bd48f309dfaf5edf1aa0e704e9dcf
juansealvarez <juansealvarez5@gmail.com>
2021-07-08 19:06:14 -0500

    Feat: Carga inicial

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const daoCategorias = require('./models/dao_categorias');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname,'assets'))); //configurar archivos estaticos
 
